6e81ea1689636181d6131d0845f4abcd717998d1
Jose Gonzalez <jozho.g@gmail.com>
2019-10-09 20:27:24 -0300

    trabajando tipos de datos

---
+++ dataTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace dataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Byte");	
+        	// Console.WriteLine("Minimo: " + byte.MinValue );
+        	// Console.WriteLine("Maximo: " + byte.MaxValue);
+        	// Console.WriteLine("sByte");	
+        	// Console.WriteLine("Minimo: " + sbyte.MinValue);
+        	// Console.WriteLine("Maximo: " + sbyte.MaxValue);
+        	// Console.WriteLine("Short");	
+        	// Console.WriteLine("Minimo: " + short.MinValue);
+        	// Console.WriteLine("Maximo: " + short.MaxValue);
+        	// Console.WriteLine("Ushort");	
+        	// Console.WriteLine("Minimo: " + ushort.MaxValue);
+        	// Console.WriteLine("Maximo: " + ushort.MaxValue);
+        	// Console.WriteLine("Uint");
+        	// Console.WriteLine("Minimo: " + uint.MaxValue);
+        	// Console.WriteLine("Maximo: " + uint.MaxValue);
+        	// Console.WriteLine("Ulong");	
+        	// Console.WriteLine("Maximo: " + ulong.MaxValue);
+        	// Console.WriteLine("Maximo: " + ulong.MaxValue);
+			Console.WriteLine("Float");	
+        	Console.WriteLine("Minimo: " + float.MinValue );
+        	Console.WriteLine("Maximo: " + float.MaxValue);
+        	Console.WriteLine("Double");	
+        	Console.WriteLine("Minimo: " + Double.MinValue);
+        	Console.WriteLine("Maximo: " + Double.MaxValue);
+        	Console.WriteLine("Decimal");	
+        	Console.WriteLine("Minimo: " + Decimal.MinValue);
+        	Console.WriteLine("Maximo: " + Decimal.MaxValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
